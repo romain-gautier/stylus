fd7fc1ffa895834c2e664ff2dec12df8957a9c2c
Romain Gautier <romain.photog@gmail.com>
2015-12-13 16:04:29 +0100

    navbar dynamique

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,16 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+
+  $(window).on("scroll", function(){
+    if ( $(this).scrollTop() > 80 ){
+      $(".navbar").css("background-color", "black");
+    } else {
+      $(".navbar").css("background-color", "transparent");
+    }
+  });
+
+
+});
